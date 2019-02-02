deab2226b2a31db185b539dbf5a8af42d3be65d9
iDarkDuck <nevinganesan@hotmail.com>
2019-02-01 21:08:23 -0500

    Initial Commit

---
+++ C++ Animated Wallpaper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Animated Wallpaper
+//
+//  Created by iDarkDuck on 2019-02-01.
+//  Copyright © 2019 iDarkDuck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
